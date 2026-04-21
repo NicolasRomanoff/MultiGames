8b95b2ccc274deb3e346fd25cb5ace02e106b417
NicolasR <nicolas.romanoff94@gmail.com>
2026-04-21 03:59:42 +0200

    remove: useless mock classes

--- apps/server/__tests__/classes/game-manager/gameManager.mock.ts
+++
@@ -1,9 +0,0 @@
-import { vi } from "vitest";
-import type { IGameManager } from "../../../src/classes/game-manager/IGameManager.js";
-
-export const getGameManagerMock = () => {
-  return {
-    addGame: vi.fn(),
-    findGame: vi.fn(),
-  } as IGameManager;
-};
